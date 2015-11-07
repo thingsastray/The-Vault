8cdfa96e6340510d0e453feed7cc5920e19cf8c5
thingsastray <andrewpeterson808@gmail.com>
2015-11-07 12:14:15 -1000

    Finsihed

--- vault.js
+++ vault.js
@@ -1,4 +1,17 @@
+
 'use strict';
 module.exports = function() {
-  
+	var vault = {
+		setValue: function (key, value){
+			vault.key = value;
+		},
+		getValue: function (key) {
+			if(vault.hasOwnProperty('key')){
+				return vault.key;
+			}else {
+				return null;
+			}
+		}
+	};
+return vault;
 };
